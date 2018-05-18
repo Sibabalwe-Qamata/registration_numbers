516d518d2d89e241a9551fb39e7f8670944df3b7
Sibabalwe Qamata <prof.qamata@gmail.com>
2018-05-18 12:29:29 +0200

    Setting-up testing environment

---
+++ test/registrationNumber-test.js
@@ -0,0 +1,7 @@
+describe('The Greet function', function() 
+{
+  it('It should Greet the user (Siba) with the proper language and name (i.e Hello, Siba)',
+    function() {
+      var numbers = numberPlates({});
+     });
+  });
