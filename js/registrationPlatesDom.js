89c30dde9f024d1abd3e525940fcc3efdfd8a9b8
Sibabalwe-Qamata <prof.qamata@gmail.com>
2018-05-21 18:46:50 +0200

    Changes made

--- js/registrationPlatesDom.js
+++ js/registrationPlatesDom.js
@@ -3,75 +3,51 @@ document.addEventListener('DOMContentLoaded', function () {
     var regNumText = document.querySelector(".regNumber");
     var addBtn = document.querySelector(".add");
     var showBtn = document.querySelector(".show");
-    var DisplayPlates = document.querySelector(".displayPlates");
+    var DisplayPlates = document.querySelector(".list-plates");
+
    
 
     //Below to get the stored users from local storage
-    var storedUserList = localStorage.getItem('Name') ? JSON.parse(localStorage.getItem('Plate')) : {};
-    var namesToStore = greeting(storedUserList);
+    //var storedUserList = localStorage.getItem('Name') ? JSON.parse(localStorage.getItem('Plate')) : {};
+    //var namesToStore = greeting(storedUserList);
     //displayCounter.innerHTML = namesToStore.counter();
-
-    //Check if there is anything in LocalStorage which is a Map....
-    /**function verifyMap() {
-        if (localStorage.getItem('Array', JSON.parse(localStorage.getItem('Name'))) === undefined) {
-            localStorage.setItem("ArrayList:", []);
-        }
-    }**/
+    var RegList = [];
+  
 
 
     function showNumberPlates() {
-        //var checkedRadioBtn = document.querySelector("input[name='language']:checked");
-        let numPlate = regNumText.value.trim();  
-
-         if(numPlate)
-         {
-             /**if(checkedRadioBtn !== null)
-             {
-                     
-                var nameInCaps = namesToStore.capitilize(nameOfPerson);
-                var nameToset = namesToStore.set_Name(nameInCaps);
-                 languageType = checkedRadioBtn.value;
-                var languageToUse = namesToStore.set_language(languageType);
-
-                namesToStore.get_name();
-                var newMapList = namesToStore.get_NameList();
-                var greetMessage = namesToStore.doGreet();
-       
-                 localStorage.setItem("Name", JSON.stringify(newMapList));
-
-                 displayCounter.innerHTML = namesToStore.counter();
-                 DisplayGreet.innerHTML = greetMessage;
-
-
-             }**/
-         }
-        if(numPlate === "")
-         {
-             if((languageType === '') || (languageType === null) ){
-                DisplayGreet.innerHTML = "Please enter a name and choose a language!";
-             }
+        
+        let numPlate = regNumText.value;
+        var numPlateFormat = numPlate.toUpperCase(); 
+        var regNumbersToStore = document.createElement('regNumbersToStore');
+       // console.log(numPlate);
+        
+        //Adding numberPlates dynamically
+        if(numPlate.length > 0 && numPlate !== null)
+        {
             
-         }
 
-         if(typeof(numPlate) != null){
+            //To Display the registration numbers in the list
+            var newDisplay = document.createElement("div"); 
+            // and give it some content 
+            var newContent = document.createTextNode(numPlateFormat); 
 
-            if((languageType === '') || (languageType === null) ){
-                DisplayGreet.innerHTML = "Please enter a name and choose a language!";
-             }
-         }
+            console.log(newContent);
+            // add the text node to the newly created div
+            newDisplay.appendChild(newContent);  
 
+        }
+
+
+       
     }
 
-
-    resetBtn.addEventListener('click', function () {
+    /**showBtn.addEventListener('click', function () {
         
-        window.location.reload();
-        localStorage.clear();
-        displayCounter.innerHTML = 0;
-    });
+      
+    });**/
 
-    greetBtn.addEventListener('click', function () {
-        verifyMap();
-        greetPerson();
+    addBtn .addEventListener('click', function () {
+      showNumberPlates();
     });
 });
