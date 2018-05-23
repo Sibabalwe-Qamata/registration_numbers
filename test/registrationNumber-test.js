b7008b8e1c89f0664ba20cc984449feb39d43758
Sibabalwe Qamata <prof.qamata@gmail.com>
2018-05-23 11:52:55 +0200

    Added test scenarios

--- test/registrationNumber-test.js
+++ test/registrationNumber-test.js
@@ -1,7 +1,33 @@
 describe('The Registration Number Plates function', function() 
 {
-  it('It should return the Number plates of the Town Given.',
+   
+    it('It should return the number plates array of the Town Given (Cape Town).',
     function() {
+
       var numbers = numberPlates({});
      });
+
+     it('It should return the number plates array of the Town Given (Paarl).',
+     function() {
+
+       var numbers = numberPlates({});
+      });
+
+      it('It should return the number plates array of the Town Given (Worcester).',
+      function() {
+
+        var numbers = numberPlates({});
+       });
+
+
+      it('It should return the number plates that were stored that were from the Western Cape.',
+       function() {
+       var numbers = numberPlates({});
+       });
+
+       it('It should return the false for number plates that are not from the Western Cape.',
+       function() {
+       var numbers = numberPlates({});
+       });
+
   });
